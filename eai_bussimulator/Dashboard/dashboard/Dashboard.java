e21aae6b738400771b44955c96ccbeb81e9f5cfa
bucusor <bucusor@hotmail.com>
2023-01-16 14:09:49 +0100

    Code unit size Dashboard

--- eai_bussimulator/Dashboard/dashboard/Dashboard.java
+++ eai_bussimulator/Dashboard/dashboard/Dashboard.java
@@ -42,38 +42,42 @@ public class Dashboard extends Application {
 	@Override // Override the start method in the Application class
 	public void start(Stage primaryStage) {
 		// Create a pane and set its properties
+		TextField halteInput = new TextField("A-Z");
+		TextField richting = new TextField("1 of -1");
+		GridPane pane = createPane(halteInput, richting);
+		pane = addButton(pane, halteInput, richting);
+		Scene scene = new Scene(pane);
+		primaryStage.setTitle("BusSimulatie control-Center");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public GridPane createPane(TextField halteInput, TextField richting) {
 		GridPane pane = new GridPane();
 		pane.setAlignment(Pos.CENTER);
 		pane.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
 		pane.setHgap(5.5);
 		pane.setVgap(5.5);
 		pane.add(new Label("Halte:"), 0, 0);
-		TextField halteInput = new TextField("A-Z");
 		pane.add(halteInput, 1, 0);
 		pane.add(new Label("Richting:"), 0, 1);
-		TextField richting = new TextField("1 of -1");
 		pane.add(richting, 1, 1);
+		return pane;
+	}
+
+	public GridPane addButton(GridPane pane, TextField halteInput, TextField richting) {
 		Button btBord = new Button("Start Bord");
-		btBord.setOnAction(e -> {
-			startBord(halteInput.getText(), richting.getText());
-		});
+		btBord.setOnAction(e -> {startBord(halteInput.getText(), richting.getText());});
 		Button btStart = new Button("Start");
-		btStart.setOnAction( e -> {
-			startAlles();
-		});
+		btStart.setOnAction( e -> {startAlles();});
 		Button btLogger = new Button("Start Logger");
-		btLogger.setOnAction( e -> {
-			thread(new ArrivaLogger(), false);
-		});
+		btLogger.setOnAction( e -> {thread(new ArrivaLogger(), false);});
 		pane.add(btBord, 1, 5);
 		pane.add(btStart, 2, 5);
 		pane.add(btLogger, 3, 5);
 		GridPane.setHalignment(btBord, HPos.LEFT);
 		GridPane.setHalignment(btStart, HPos.LEFT);
 		GridPane.setHalignment(btLogger, HPos.LEFT);
-		Scene scene = new Scene(pane);
-		primaryStage.setTitle("BusSimulatie control-Center");
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		return pane;
 	}
 } 
