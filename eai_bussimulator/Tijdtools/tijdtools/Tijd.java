62051b819417e9c469d6f7646f18e727b92f077b
bucusor <bucusor@hotmail.com>
2023-01-18 14:41:11 +0100

    Module coupling between Tijd and TijdFuncties

--- eai_bussimulator/Tijdtools/tijdtools/Tijd.java
+++ eai_bussimulator/Tijdtools/tijdtools/Tijd.java
@@ -1,17 +1,16 @@
 package tijdtools;
 
-public class Tijd
-{
+public class Tijd implements Time{
 	private int uur;
 	private int minuut;
 	private int seconde;
-	
+
 	public Tijd(){
 		this.uur = 0;
 		this.minuut = 0;
-		this.seconde = 0;		
+		this.seconde = 0;
 	}
-	
+
 	public Tijd(int uur, int minuut, int seconde) {
 		super();
 		this.uur = uur;
@@ -19,34 +18,41 @@ public class Tijd
 		this.seconde = seconde;
 	}
 
+	@Override
 	public int getUur() {
 		return uur;
 	}
 
+	@Override
 	public void setUur(int uur) {
 		this.uur = uur;
 	}
 
+	@Override
 	public int getMinuut() {
 		return minuut;
 	}
 
+	@Override
 	public void setMinuut(int minuut) {
 		this.minuut = minuut;
 	}
 
+	@Override
 	public int getSeconde() {
 		return seconde;
 	}
 
+	@Override
 	public void setSeconde(int seconde) {
 		this.seconde = seconde;
 	}
-	
-	public void increment(Tijd step){
-		this.seconde += step.seconde;
-		this.minuut += step.minuut;
-		this.uur += step.uur;
+
+	@Override
+	public void increment(Time step) {
+		this.seconde += step.getSeconde();
+		this.minuut += step.getMinuut();
+		this.uur += step.getUur();
 		if (this.seconde>=60){
 			this.seconde-=60;
 			this.minuut++;
@@ -57,12 +63,13 @@ public class Tijd
 		}
 	}
 
-	public Tijd copyTijd(){
+	@Override
+	public Time copyTijd() {
 		return new Tijd(this.uur, this.minuut, this.seconde);
 	}
-	
+
 	@Override
 	public String toString() {
 		return String.format("%02d:%02d:%02d", uur,minuut,seconde);
-	}    	
+	}
 }
--- eai_bussimulator/Tijdtools/tijdtools/TijdFuncties.java
+++ eai_bussimulator/Tijdtools/tijdtools/TijdFuncties.java
@@ -4,37 +4,37 @@ import java.io.IOException;
 import com.thoughtworks.xstream.XStream;
 
 public class TijdFuncties {
-	private Tijd startTijd;
-	private Tijd simulatorTijd;
-	private Tijd verschil;
+	private Time startTijd;
+	private Time simulatorTijd;
+	private Time verschil;
 	private int interval;
 	private int syncInterval;
 	private int syncCounter;
-	
-    public void initSimulatorTijden(int interval, int syncInterval){
-    	simulatorTijd=new Tijd(0,0,0);
-    	startTijd=getCentralTime();
-    	verschil=berekenVerschil(startTijd,simulatorTijd);
-    	this.interval=interval;
-    	this.syncCounter=syncInterval;
-    	this.syncInterval=syncInterval;
-    }
 
-    public String getSimulatorWeergaveTijd(){
-    	Tijd simulatorWeergaveTijd= simulatorTijd.copyTijd();
-    	simulatorWeergaveTijd.increment(verschil);
-    	return simulatorWeergaveTijd.toString();
-    }
-    
-    public int getCounter(){
-    	return calculateCounter(simulatorTijd);
-    }
-    
-    public int getTijdCounter(){
-    	return calculateCounter(simulatorTijd)+calculateCounter(verschil);    	
-    }
-    
-    public void simulatorStep() throws InterruptedException{
+	public void initSimulatorTijden(int interval, int syncInterval){
+		simulatorTijd=new Tijd(0,0,0);
+		startTijd=getCentralTime();
+		verschil=berekenVerschil(startTijd,simulatorTijd);
+		this.interval=interval;
+		this.syncCounter=syncInterval;
+		this.syncInterval=syncInterval;
+	}
+
+	public String getSimulatorWeergaveTijd(){
+		Time simulatorWeergaveTijd= simulatorTijd.copyTijd();
+		simulatorWeergaveTijd.increment(verschil);
+		return simulatorWeergaveTijd.toString();
+	}
+
+	public int getCounter(){
+		return calculateCounter(simulatorTijd);
+	}
+
+	public int getTijdCounter(){
+		return calculateCounter(simulatorTijd)+calculateCounter(verschil);
+	}
+
+	public void simulatorStep() throws InterruptedException{
 		Thread.sleep(interval);
 		simulatorTijd.increment(new Tijd(0,0,1));
 		syncCounter--;
@@ -42,49 +42,49 @@ public class TijdFuncties {
 			syncCounter=syncInterval;
 			synchroniseTijd();
 		}
-    }
-    
-    private int calculateCounter(Tijd tijd){
-    	return tijd.getUur()*3600+tijd.getMinuut()*60+tijd.getSeconde();
-    }
-    
-    private Tijd berekenVerschil(Tijd reverentieTijd, Tijd werkTijd){
-    	int urenVerschil = reverentieTijd.getUur()-werkTijd.getUur();
-    	int minutenVerschil = reverentieTijd.getMinuut()-werkTijd.getMinuut();
-    	int secondenVerschil = reverentieTijd.getSeconde()-werkTijd.getSeconde();
-    	if (secondenVerschil<0){
-    		minutenVerschil--;
-    		secondenVerschil+=60;
-    	}
-    	if (minutenVerschil<0){
-    		urenVerschil--;
-    		minutenVerschil+=60;
-    	}
-    	return new Tijd(urenVerschil, minutenVerschil, secondenVerschil);
-    }
-    
-    private void synchroniseTijd(){
-    	Tijd huidigeTijd = getCentralTime();
-    	System.out.println("De werkelijke tijd is nu: "+ huidigeTijd.toString());
-    	Tijd verwachtteSimulatorTijd = simulatorTijd.copyTijd();
-    	verwachtteSimulatorTijd.increment(verschil);
-    	Tijd delay = berekenVerschil(huidigeTijd, verwachtteSimulatorTijd);
-    	verschil.increment(delay);
-    }
-    
-    private Tijd getCentralTime()
-    {
-    	try {
-    		HTTPFuncties httpFuncties = new HTTPFuncties();
-			String result = httpFuncties.executeGet("xml");
-	        XStream xstream = new XStream();
-	        xstream.alias("Tijd", Tijd.class);
-	        Tijd tijd=(Tijd)xstream.fromXML(result);
-	        return tijd;
+	}
 
-    	} catch (IOException e) {
+	private int calculateCounter(Time tijd){
+		return tijd.getUur()*3600+tijd.getMinuut()*60+tijd.getSeconde();
+	}
+
+	private Time berekenVerschil(Time reverentieTijd, Time werkTijd){
+		int urenVerschil = reverentieTijd.getUur()-werkTijd.getUur();
+		int minutenVerschil = reverentieTijd.getMinuut()-werkTijd.getMinuut();
+		int secondenVerschil = reverentieTijd.getSeconde()-werkTijd.getSeconde();
+		if (secondenVerschil<0){
+			minutenVerschil--;
+			secondenVerschil+=60;
+		}
+		if (minutenVerschil<0){
+			urenVerschil--;
+			minutenVerschil+=60;
+		}
+		return new Tijd(urenVerschil, minutenVerschil, secondenVerschil);
+	}
+
+	private void synchroniseTijd(){
+		Time huidigeTijd = getCentralTime();
+		System.out.println("De werkelijke tijd is nu: "+ huidigeTijd.toString());
+		Time verwachtteSimulatorTijd = simulatorTijd.copyTijd();
+		verwachtteSimulatorTijd.increment(verschil);
+		Time delay = berekenVerschil(huidigeTijd, verwachtteSimulatorTijd);
+		verschil.increment(delay);
+	}
+
+	private Time getCentralTime()
+	{
+		try {
+			HTTPFuncties httpFuncties = new HTTPFuncties();
+			String result = httpFuncties.executeGet("xml");
+			XStream xstream = new XStream();
+			xstream.alias("Tijd", Tijd.class);
+			Time tijd=(Tijd)xstream.fromXML(result);
+			return tijd;
+
+		} catch (IOException e) {
 			e.printStackTrace();
 			return new Tijd(0,0,0);
 		}
-    }
+	}
 }
